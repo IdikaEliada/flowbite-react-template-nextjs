a5aedef87e634ee98a22415f081d0b71a5d3b67a
IdikaEliada <idika1996@gmail.com>
2025-11-05 00:06:48 +0100

    signup page

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -2,11 +2,15 @@
 
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
+import { DarkThemeToggle } from 'flowbite-react'
+import { HiArrowCircleLeft } from 'react-icons/hi'
 import Link from 'next/link'
 
 
 export default function CreateAccount() {
   const router = useRouter()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   const [formData, setFormData] = useState({
     email: '',
     password: '',
@@ -14,6 +18,7 @@ export default function CreateAccount() {
     username: ''
   })
   const [error, setError] = useState('')
+
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
@@ -48,76 +53,102 @@ export default function CreateAccount() {
   }
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div className="w-full max-w-md space-y-8">
-        <div>
-          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight">
-            Create your account
-          </h2>
-        </div>
-        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-          {error && (
-            <div className="text-red-500 text-center">{error}</div>
-          )}
-          <div className="space-y-4 rounded-md shadow-sm">
-            <div>
-              <input
-                type="text"
-                required
-                className="relative block w-full rounded-md border-0 p-2"
-                placeholder="Username"
-                value={formData.username}
-                onChange={(e) => setFormData({...formData, username: e.target.value})}
-              />
-            </div>
-            <div>
-              <input
-                type="email"
-                required
-                className="relative block w-full rounded-md border-0 p-2"
-                placeholder="Email address"
-                value={formData.email}
-                onChange={(e) => setFormData({...formData, email: e.target.value})}
-              />
-            </div>
-            <div>
-              <input
+    <main className="flex max-h-screen flex-col items-center justify-center pt-5 pb-38">
+          <div className="absolute top-4 right-4 left-4 flex items-center justify-between ">
+            <Link href="/" className="flex items-center gap-2" >
+              <HiArrowCircleLeft className="dark:text-gray-300 text-xl cursor-pointer text-[#111928]"/>
+              <span className="relative w-fit text-xs font-semibold whitespace-nowrap text-[#111928] dark:text-white cursor-pointer">
+                Back
+              </span>
+            </Link>
+            <DarkThemeToggle className="cursor-pointer"/>
+          </div>
+    
+          <div className="flex flex-col items-center justify-center w-full min-w-sm max-w-md px-4 py-8  sm:max-w-sm xl:p-0 ">
+            
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white pb-4">
+              Create your account
+            </h1>
+    
+            
+    
+            <div className="backdrop-blur-md bg-white/75 rounded-xl shadow dark:border-2 s:mx-w-sm dark:bg-gray-800 dark:border-gray-700/15 px-4 py-6">
+              <div className="p-4 space-y-2 md:space-y-4 sm:p-8">
+
+                <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+                  <div>
+                    {error && (
+                      <div className="text-red-500 text-center">{error}</div>
+                    )}
+                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Your email
+                    </label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      placeholder="name@company.com"
+                      required
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+    
+                  <div>
+                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="••••••••"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      required
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                      Password
+                    </label>
+                  <input
                 type="password"
+                name="confirm-password"
+                id="confirm-password"
                 required
-                className="relative block w-full rounded-md border-0 p-2"
-                placeholder="Password"
-                value={formData.password}
-                onChange={(e) => setFormData({...formData, password: e.target.value})}
-              />
-            </div>
-            <div>
-              <input
-                type="password"
-                required
-                className="relative block w-full rounded-md border-0 p-2"
-                placeholder="Confirm Password"
+                className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                 placeholder="Confirm Password"
                 value={formData.confirmPassword}
                 onChange={(e) => setFormData({...formData, confirmPassword: e.target.value})}
               />
+              </div>
+    
+                  
+    
+                  <div>
+                    <button
+                      type="submit"
+                      className="group relative flex w-full justify-center rounded-md bg-primary-600 px-3 py-2 text-sm font-semibold text-white hover:bg-primary-500"
+                    >
+                      Sign up
+                    </button>
+                  </div>
+    
+                  
+                </form>
+              
+                <div className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  Already have an account?{' '}
+                  <Link href="/login"  className="font-medium text-primary-600 hover:underline dark:text-primary-500">
+                    Login here
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
-
-          <div>
-            <button
-              type="submit"
-              className="group relative flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white hover:bg-blue-500"
-            >
-              Sign up
-            </button>
-          </div>
-        </form>
-        <div className="text-center">
-          Already have an account?{' '}
-          <Link href="/login" className="text-blue-600 hover:text-blue-500">
-            Login here
-          </Link>
-        </div>
-      </div>
-    </div>
+        </main>
   )
 }
