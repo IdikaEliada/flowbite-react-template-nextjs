3058b2456874677f347fa474fb14c3f995e6ac63
IdikaEliada <idika1996@gmail.com>
2025-11-04 23:08:54 +0100

    hello

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -45,7 +45,7 @@ export default function LoginPage() {
           Bookmate
         </Link>
 
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-sm xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Sign in to your account
@@ -103,7 +103,7 @@ export default function LoginPage() {
                   </div>
                 </div>
 
-                <Link href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">
+                <Link href="/forgot-password" className="text-sm font-medium dark:text-primary-400 text-primary-600 hover:underline dark:text-primary-500">
                   Forgot password?
                 </Link>
               </div>
