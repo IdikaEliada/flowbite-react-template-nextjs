e0f1a39e6e296efc20c79158d4b52bbdf0163705
IdikaEliada <idika1996@gmail.com>
2025-11-04 16:40:07 +0100

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
             transition={{ type: "spring", stiffness: 120, damping: 14 }}
             className="relative text-center text-5xl leading-[95%] tracking-tight font-semibold text-gray-900 dark:text-gray-200"
           >
-            Welcome to <span className="font-black tracking-wider dark:bg-linear-65 from-primary-100 to-primary-300 bg-linear-65 from-primary-400 to-primary-600 text-transparent [background-clip:text] [-webkit-background-clip:text]" >Bookmate </span>
+            Welcome to <span className="font-black tracking-wider dark:bg-gradient-to-br from-primary-100 via-primary-200 to-primary-300 bg-gradient-to-br from-primary-400 via-primary-500 to-primary-600 text-transparent [background-clip:text] [-webkit-background-clip:text]" >Bookmate </span>
           </motion.h1>
 
           <motion.span
